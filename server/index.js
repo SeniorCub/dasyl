f9a253d89ea26fa1e8ae2e0692ba820abf8a7689
seniorcub <farinderif@gmail.com>
2026-07-13 23:01:00 +0100

    chore: switch deployment from render to vercel

--- server/index.js
+++ server/index.js
@@ -75,6 +75,10 @@ app.get('/api/leaderboard', async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+module.exports = app;
